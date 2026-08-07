7cd32add897bf25eff4a31c8c7a026f439a7a72c
Vinicius Nery <vinicius_nery2@hotmail.com>
2026-08-07 17:30:54 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿var vendasCarros = new Dictionary<string, List<int>> {
+    { "Bugatti Veyron", new List<int> { 10, 15, 12, 8, 5 } },
+    { "Koenigsegg Agera RS", new List<int> { 2, 3, 5, 6, 7 } },
+    { "Lamborghini Aventador", new List<int> { 20, 18, 22, 24, 16 } },
+    { "Pagani Huayra", new List<int> { 4, 5, 6, 5, 4 } },
+    { "Ferrari LaFerrari", new List<int> { 7, 6, 5, 8, 10 } }
+};
+
+double mediaVendasBugatti = vendasCarros["Bugatti Veyron"].Average();
+Console.WriteLine("Média de vendas do Bugatti Veyron: " + mediaVendasBugatti);
+
+double mediaVendasAventador = vendasCarros["Lamborghini Aventador"].Average();
+Console.WriteLine("Média de vendas da Lamborghini Aventador: " + mediaVendasAventador);
